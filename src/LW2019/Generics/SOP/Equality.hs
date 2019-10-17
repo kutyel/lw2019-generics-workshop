2961dcbebca3cc3de143a5f3288bd01e90f12eda
kutyel <flaviocorpa@gmail.com>
2019-10-17 13:27:21 +0200

    fix ex 4! 🚀

--- src/LW2019/Generics/SOP/Equality.hs
+++ src/LW2019/Generics/SOP/Equality.hs
@@ -1,36 +1,36 @@
-{-# LANGUAGE KindSignatures   #-}
-{-# LANGUAGE RankNTypes       #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs            #-}
+{-# LANGUAGE KindSignatures   #-}
+{-# LANGUAGE RankNTypes       #-}
+
 module LW2019.Generics.SOP.Equality where
 
-import LW2019.Types.Regular
-import LW2019.Generics.SOP.Repr
-import Generics.SOP
+import           Generics.SOP
+import           LW2019.Generics.SOP.Repr
+import           LW2019.Types.Regular
 
 -- Recall the definition of NS and NP.
-
 -- Consider that:
 --
 -- > type family All p '[]       = ()
 -- > type family All p (c ': cs) = (p c , All p cs)
--- > 
+-- >
 -- > type family All2 p c        = All (All p) c
 --
-
-geq :: (Generic a , All2 Eq (Code a))
-    => a -> a -> Bool
+geq :: (Generic a, All2 Eq (Code a)) => a -> a -> Bool
 geq x y = go (from x) (from y)
   where
     go :: (All2 Eq xss) => SOP I xss -> SOP I xss -> Bool
-    go (SOP (Z xs))  (SOP (Z ys))  = and . hcollapse $ hczipWith p eq xs ys
+    go (SOP (Z xs)) (SOP (Z ys))   = and . hcollapse $ hczipWith p eq xs ys
     go (SOP (S xss)) (SOP (S yss)) = go (SOP xss) (SOP yss)
-    go _             _             = False
-
+    go _ _                         = False
     p :: Proxy Eq
     p = Proxy
-
-    eq :: forall (a :: *). Eq a => I a -> I a -> K Bool a
+    eq ::
+         forall (a :: *). Eq a
+      => I a
+      -> I a
+      -> K Bool a
     eq (I a) (I b) = K (a == b)
 
 -- Now, we need to instruct GHC to use the generic equality
@@ -39,7 +39,6 @@ geq x y = go (from x) (from y)
 --
 -- If you struggle to see why, dry-run 'geq (Node1 42 Leaf) (Node1 42 Leaf)'
 -- on pen-and-paper, whilst keeping track of the instance resolution.
-
 instance Eq QualName where
   x == y = geq x y
 
--- src/LW2019/Generics/SOP/Repr.hs
+++ src/LW2019/Generics/SOP/Repr.hs
@@ -1,35 +1,30 @@
+{-# LANGUAGE DataKinds          #-}
 {-# LANGUAGE DeriveGeneric      #-}
 {-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE TypeFamilies       #-}
-{-# LANGUAGE DataKinds          #-}
 {-# LANGUAGE TemplateHaskell    #-}
+{-# LANGUAGE TypeFamilies       #-}
+
 module LW2019.Generics.SOP.Repr where
 
 -- The types we want to look at are:
-import LW2019.Types.Regular
+import           LW2019.Types.Regular
 
 -- Our generic library of study here will be:
-import Generics.SOP
-import Generics.SOP.TH
+import           Generics.SOP
+import           Generics.SOP.TH
 
+-- Only one constructor with three fields
 instance Generic BookInfo where
-  -- Only one constructor with three fields
-  type Code BookInfo = '[ '[String , String , Float] ]
-
-  to (SOP (Z (I name :* I isbn :* I price :* Nil)))
-    = BookInfo name isbn price
-  from (BookInfo name isbn price)
-    = (SOP (Z (I name :* I isbn :* I price :* Nil)))
-
-data Ex4_A
+  type Code BookInfo = '[ '[ String, String, Float]]
+  to (SOP (Z (I name :* I isbn :* I price :* Nil))) = BookInfo name isbn price
+  from (BookInfo name isbn price) = SOP (Z (I name :* I isbn :* I price :* Nil))
 
 instance Generic QualName where
-  type Code QualName = Ex4_A
-
-  to (SOP    (Z (I b :* Nil)))         = _ex4_b
-  to (SOP (S (Z (I b :* I r :* Nil)))) = _ex4_c
-
-  from = _ex4_d
+  type Code QualName = '[ '[ String], '[ String, QualName]]
+  to (SOP (Z (I b :* Nil)))            = Base b
+  to (SOP (S (Z (I b :* I r :* Nil)))) = Qual b r
+  from (Base b)   = SOP (Z (I b :* Nil))
+  from (Qual b r) = SOP (S (Z (I b :* I r :* Nil)))
 
 -- We can also use template haskell to derive our instances.
 -- What's the generated code? Again, we can check by
