e6f418db3097b956e8daabb9bccd6af9576308c5
kutyel <flaviocorpa@gmail.com>
2019-10-17 12:53:55 +0200

    fix ex2

--- src/LW2019/Generics/GHC/Repr.hs
+++ src/LW2019/Generics/GHC/Repr.hs
@@ -1,21 +1,19 @@
 {-# LANGUAGE DeriveGeneric      #-}
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE FlexibleContexts   #-}
 {-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeFamilies       #-}
 {-# LANGUAGE TypeOperators      #-}
-{-# LANGUAGE FlexibleContexts   #-}
-{-# LANGUAGE DefaultSignatures  #-}
-{-# LANGUAGE KindSignatures     #-}
+
 module LW2019.Generics.GHC.Repr where
 
 -- The types we want to look at are:
-import LW2019.Types.Regular
+import           LW2019.Types.Regular
 
 -- Our generic library of study here will be:
-import GHC.Generics
+import           GHC.Generics
 
 -- * Generic Representation of Regular Types
-
 -- |First and foremost; lets define our 'BookInfo' datatype
 -- as an instance of 'Generic'.
 --
@@ -25,21 +23,16 @@ import GHC.Generics
 --  3. The 'from' function
 instance Generic BookInfo where
   type Rep BookInfo = K1 R String :*: K1 R String :*: K1 R Float
-
-  to (K1 name :*: K1 isbn :*: K1 price)
-    = BookInfo name isbn price
-
-  from (BookInfo name isbn price)
-    = K1 name :*: K1 isbn :*: K1 price
+  to (K1 name :*: K1 isbn :*: K1 price) = BookInfo name isbn price
+  from (BookInfo name isbn price) = K1 name :*: K1 isbn :*: K1 price
 
 -- Let's look into 'QualName':
 instance Generic QualName where
   type Rep QualName = K1 R String :+: (K1 R String :*: K1 R QualName)
-
-  to (L1 (K1 name))          = _ex2_a
-  to (R1 (K1 q :*: K1 rest)) = _ex2_b
-
-  from = _ex2_c
+  to (L1 (K1 name))          = Base name
+  to (R1 (K1 q :*: K1 rest)) = Qual q rest
+  from (Base name)   = L1 $ K1 name
+  from (Qual q rest) = R1 $ K1 q :*: K1 rest
 
 -- GHC can infer these boring instances for us, though:
 deriving instance Generic (Tree12 a)
@@ -54,8 +47,5 @@ deriving instance Generic (Tree12 a)
 -- things, ie, the name of the original constructors.
 -- Can you distil a simpler way of writing that type?
 -- Bear in mind that "Rec0" is just "K1 R":
-
-type Tree12Rep a 
-  = Ex2_D
-
-data Ex2_D
+type Tree12Rep a
+   = U1 :+: (K1 R a :*: K1 R (Tree12 a)) :+: (K1 R a :*: K1 R (Tree12 a) :*: K1 R (Tree12 a))
--- src/LW2019/Types/Regular.hs
+++ src/LW2019/Types/Regular.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveFunctor #-}
+
 -- |Here we will find regular datatypes to
 -- experiment with. Some with recursion,
 -- some without recursion.
@@ -6,10 +7,11 @@ module LW2019.Types.Regular where
 
 -- |As our first example we look into a non-recursive
 -- datatype, call it 'BookInfo'.
-data BookInfo
-  = BookInfo String -- ^ Name
-             String -- ^ ISBN
-             Float  -- ^ Price
+data BookInfo =
+  BookInfo
+    String -- ^ Name
+    String -- ^ ISBN
+    Float -- ^ Price
   deriving (Show)
 
 -- |Lets make a slightly more interesting datatype
@@ -24,5 +26,4 @@ data Tree12 a
   = Leaf
   | Node1 a (Tree12 a)
   | Node2 a (Tree12 a) (Tree12 a)
-  deriving (Show , Functor)
-
+  deriving (Show, Functor)
